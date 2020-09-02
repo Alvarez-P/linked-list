eeb5012a64b72f9014dd0185eaf1643a05118459
Esteban Alvarez <aeap19980929@gmail.com>
2020-09-02 10:47:04 -0500

    first commit
    * Agregando codigo
    * Agregando README

---
+++ README.md
@@ -0,0 +1,53 @@
+# Linked List
+
+Example program for using linked lists in Python
+
+## Getting Started :arrow_forward:
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites :clipboard:
+
+What things you need to install the software and how to install them
+
+- [Python](https://www.python.org/)
+
+### Installing :arrow_down:
+
+No requirements to install
+
+## Deployment :package:
+
+Run script with:
+```sh
+python3 index.py
+```
+
+## Built With :hammer_and_wrench:
+
+* [Python](https://www.python.org/)
+
+## Versioning :triangular_flag_on_post:
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Future work :rocket:
+
+## Contributors :family_man_man_boy:
+
+- **Esteban Alvarez** - _Initial work_ - [@alvarez98](https://github.com/alvarez98)
+
+## Credits :star:
+
+- **A template to make good README.md** - _Base template_ - [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
+
+## References :link:
+
+1. [Python](https://www.python.org/)
+2. [Listas Ligadas](https://sites.google.com/site/estdatinfjiq/unidad-iii-listas-enlazadas)
+
+## License :page_facing_up:
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+Made with ❤️ by [Esteban Alvarez](https://github.com/alvarez98)
---
+++ index.py
@@ -0,0 +1,96 @@
+# Creamos la clase node
+class Node:
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+
+# Creamos la clase LinkedList
+class LinkedList(object):
+    def __init__(self, node = None):
+        """Se asigna como Nodo inicial a primero y ultimo por que apuntan al mismo nodo
+        y se define el tamagno de la lista en cero"""
+        self.__last = self.__head = node 
+        self.__length = 0
+
+    def getLength(self):
+        """Devuelve el tamaño de la lista"""
+        return self.__length
+    
+    def getLastNode(self):
+        """Devuelve el ultimo dato de la lista"""
+        return "Last: " + str(self.__last.data) 
+
+    def addAtFront(self, d):
+        """Agrega un nodo al inicio de la lista enlazada"""
+        self.__head = Node(data = d, next=self.__head)
+        self.__length += 1
+        if self.__last == None:
+            self.__last = self.__head
+            return True
+        
+    def addAtEnd(self, data):
+        """Agrega un nodo al final de la lista enlazada"""
+        if not self.__head:
+            self.__last = self.__head = Node(data=data)
+            return
+        curr = self.__head
+        while curr.next:
+            curr = curr.next
+        self.__last = curr.next = Node(data=data)
+
+    def deleteNode(self, d):
+        """Se elimina el nodo que tenga el dato d"""
+        currentNode = self.__head
+        prevNode = None
+
+        while currentNode:
+            if currentNode.data == d:
+                if prevNode:
+                    # Se asigna  al apuntador proximo del nodo anterior el proximo de nodo actual
+                    prevNode.next = currentNode.next
+                else:
+                    self.__head = currentNode.next
+                #En cualquiera de los dos casos se decrementa el tamaño de la lista.
+                self.__length -= 1
+                return True
+            else:
+                prevNode = currentNode
+                currentNode = currentNode.next
+
+    def find(self,d):
+        """Se busca d en la lista enlazada, si existe devuelve d, si no devuelve None"""
+        currentNode = self.__head
+        count = 0
+        while currentNode:
+            if currentNode.data == d:
+                return { "data": currentNode.data, "position": count }
+            else:
+                currentNode = currentNode.next
+            count+=1
+        #Si no se encuentra devuelve None.
+        return None 
+
+    def printList(self):
+        node = self.__head
+        while node != None:
+            print(node.data)
+            node = node.next
+
+
+
+ # Instancia de la clase
+linkedList = LinkedList()
+
+# Valores
+linkedList.addAtEnd(7)
+linkedList.addAtFront(5)
+linkedList.addAtEnd(8)
+linkedList.addAtEnd(9)
+linkedList.addAtEnd(3)
+linkedList.addAtFront(10)
+
+# Métodos
+print(linkedList.find(3))
+print(linkedList.getLastNode())
+linkedList.printList()
+
